4a7dfa3659366d11d43e17d2e9e9e0336b27b268
heinerbehrends <heinerbehrends@hotmail.com>
2021-11-02 10:42:24 +0100

    Added button with variants

---
+++ components/buttonStyles.tsx
@@ -0,0 +1,35 @@
+import { styled } from '../stitchesConfig';
+
+export const Button = styled('button', {
+  all: 'unset',
+  display: 'inline-flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRadius: 4,
+  padding: '0 15px',
+  fontSize: 15,
+  lineHeight: 1,
+  fontWeight: 500,
+  height: 35,
+
+  variants: {
+    variant: {
+      violet: {
+        backgroundColor: 'white',
+        color: '$violet.violet11',
+        boxShadow: `0 2px 10px $blackA.blackA7`,
+        '&:hover': { backgroundColor: '$mauve.mauve3' },
+        '&:focus': { boxShadow: `0 0 0 2px black` },
+      },
+      green: {
+        backgroundColor: '$green.green4',
+        color: '$green.green11',
+        '&:hover': { backgroundColor: '$green.green5' },
+        '&:focus': { boxShadow: `0 0 0 2px $green.green7` },
+      },
+    },
+  },
+  defaultVariants: {
+    variant: 'violet',
+  },
+});
