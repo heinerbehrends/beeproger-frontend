cbe6e6d950097c571984bf7f538942762d06a2e6
heinerbehrends <heinerbehrends@hotmail.com>
2021-11-02 20:21:05 +0100

    fixed colors, pointer, added IconButton

--- components/buttonStyles.tsx
+++ components/buttonStyles.tsx
@@ -11,25 +11,46 @@ export const Button = styled('button', {
   lineHeight: 1,
   fontWeight: 500,
   height: 35,
+  cursor: 'pointer',
 
   variants: {
     variant: {
       violet: {
         backgroundColor: 'white',
-        color: '$violet.violet11',
-        boxShadow: `0 2px 10px $blackA.blackA7`,
-        '&:hover': { backgroundColor: '$mauve.mauve3' },
-        '&:focus': { boxShadow: `0 0 0 2px black` },
+        color: '$violet11',
+        border: '1px solid $colors$violet8',
+        boxShadow: `0 2px 10px $colors$blackA7`,
+        '&:hover': { backgroundColor: '$colors$mauve3' },
+        '&:focus': { boxShadow: `0 0 0 2px $colors$violet11` },
       },
       green: {
-        backgroundColor: '$green.green4',
-        color: '$green.green11',
-        '&:hover': { backgroundColor: '$green.green5' },
-        '&:focus': { boxShadow: `0 0 0 2px $green.green7` },
+        backgroundColor: '$green4',
+        color: '$green11',
+        border: '1px solid $colors$green8',
+        '&:hover': { backgroundColor: '$green5' },
+        '&:focus': { boxShadow: `0 0 0 2px $colors$green7` },
       },
     },
   },
   defaultVariants: {
     variant: 'violet',
   },
+});
+
+export const IconButton = styled('button', {
+  all: 'unset',
+  fontFamily: 'inherit',
+  borderRadius: '100%',
+  height: 25,
+  width: 25,
+  display: 'inline-flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: '$violet11',
+  position: 'absolute',
+  top: 10,
+  right: 10,
+
+  '&:hover': { backgroundColor: '$violet4' },
+  '&:focus': { boxShadow: `0 0 0 2px $colors$violet7` },
 });
