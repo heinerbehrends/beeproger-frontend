462521968db544eeb1e755374984ca70f6643286
heinerbehrends <heinerbehrends@hotmail.com>
2021-11-03 12:48:41 +0100

    added edit dialogs, Button, Title, moved imageUpload

--- components/item.tsx
+++ components/item.tsx
@@ -1,8 +1,11 @@
-import React, { SetStateAction, Dispatch, FormEvent } from 'react';
-import { useState } from 'react';
+import React, { SetStateAction, Dispatch } from 'react';
 import { Item } from '../pages';
 import Checkbox from './checkbox';
+import EditDialog, { Flex } from './editDialog';
+import UploadDialog from './uploadDialog';
 import ItemImage from './itemImage';
+import { Button } from './buttonStyles';
+import Title from './title';
 
 type DisplayItemProps = {
   item: Item;
@@ -15,75 +18,22 @@ export default function ShowEditItem({
   items,
   setItems,
 }: DisplayItemProps) {
-  const [showDetails, setShowDetails] = useState(false);
-  const [foto, setFoto] = useState<File | null>(null);
-
-  function submitImage(id: number) {
-    return (event: FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-
-      const formData = new FormData();
-      if (foto === null) {
-        // set error message
-        return;
-      }
-      formData.append('foto', foto);
-      formData.append('_method', 'PATCH');
-
-      fetch(`http://localhost/api/items/${id}`, {
-        method: 'POST',
-        body: formData,
-      })
-        .then((response) => response.json())
-        .then((item) =>
-          setItems([...items.map((i) => (i.id === item.id ? item : i))])
-        );
-    };
-  }
   return (
-    <>
-      <div
-        style={{
-          display: 'grid',
-          maxWidth: '960px',
-          gridTemplateColumns: '1fr 40px 3fr 2fr 2fr 2fr',
-          padding: '1rem',
+    <React.Fragment key={item.id}>
+      <Checkbox item={item} items={items} setItems={setItems} />
+      <ItemImage item={item} />
+      <Title item={item} />
+      <Flex
+        css={{
+          height: '60px',
+          alignItems: 'center',
+          gap: '1rem',
         }}
-        key={item.id}
       >
-        <ItemImage item={item} />
-        <Checkbox item={item} items={items} setItems={setItems} />
-        <div style={{ textDecoration: item.isDone ? 'line-through' : 'none' }}>
-          {item.title}
-        </div>
-        <form
-          method="post"
-          encType="multipart/form-data"
-          onSubmit={submitImage(item.id)}
-        >
-          <div>
-            <label htmlFor="file">Add an image</label>
-            <input
-              type="file"
-              id="file"
-              name="file"
-              multiple
-              onChange={(event) => setFoto(event.target.files![0])}
-            />
-          </div>
-          <div>
-            <button>Submit</button>
-          </div>
-        </form>
+        <UploadDialog item={item} items={items} setItems={setItems} />
 
-        <button
-          style={{ width: '100px' }}
-          onClick={() => setShowDetails(!showDetails)}
-        >
-          {showDetails ? 'Hide' : 'Show'} details
-        </button>
-        <button
-          style={{ width: '100px' }}
+        <EditDialog item={item} setItems={setItems} items={items} />
+        <Button
           onClick={() => {
             setItems([...items.filter((i) => i.id !== item.id)]);
             fetch(`http://localhost/api/items/${item.id}`, {
@@ -92,18 +42,8 @@ export default function ShowEditItem({
           }}
         >
           Delete
-        </button>
-      </div>
-      <div
-        style={{
-          maxHeight: showDetails ? '18px' : '0px',
-          transition: 'max-height 0.25s ease-in',
-          overflow: 'hidden',
-          paddingLeft: '1rem',
-        }}
-      >
-        {item.details}
-      </div>
-    </>
+        </Button>
+      </Flex>
+    </React.Fragment>
   );
 }
